f5a648a92a8770318d82d0f3384d1192f6ceb33a
dorian <dekolis1218@gmail.com>
2020-01-18 14:23:22 -0800

    Add way to get input from user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static Scanner myScanner = new Scanner(System.in);
-    public static String input = myScanner.nextLine();
 
     public static void main(String[] Args) {
-        System.out.println(input);
+        System.out.println("Would you like to encode (type \'E\') or decode (type \'D\')?");
+        String type = myScanner.nextLine();
+        if (type.equals("E")) {
+            System.out.println("You chose to Encrypt a message.");
+        } else if (type.equals("D")) {
+            System.out.println("You chose to Decrypt a message.");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
