80757ed4967278488191d0b16f1ea3e9d923f9ab
dorian <dekolis1218@gmail.com>
2020-01-18 14:09:53 -0800

    Begin testing of Scanner method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] Args) {
+import java.util.Scanner;
 
+public class Main {
+
+    public static Scanner myScanner = new Scanner(System.in);
+    public static String input = myScanner.nextLine();
+
+    public static void main(String[] Args) {
+        System.out.println(input);
     }
 }
