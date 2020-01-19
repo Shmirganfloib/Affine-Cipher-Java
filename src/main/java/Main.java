492181bd273099aff3e455c78e714878dc4a8712
dorian <dekolis1218@gmail.com>
2020-01-18 17:43:06 -0800

    Fix simple word choice errors

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static Scanner myScanner = new Scanner(System.in);
 
     public static void main(String[] Args) {
-        System.out.println("Would you like to encode (type \'E\') or decode (type \'D\')?");
+        System.out.println("Would you like to encrypt (type 'E') or decrypt (type 'D')?");
         String type = myScanner.nextLine();
-        if (type.equals("E")) {
+        if (type.equals("E") | type.equals("e")) {
             System.out.println("You chose to Encrypt a message.");
-        } else if (type.equals("D")) {
+        } else if (type.equals("D") | type.equals("d")) {
             System.out.println("You chose to Decrypt a message.");
         } else {
             System.out.println("Error");
